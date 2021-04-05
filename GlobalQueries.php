4ae6d1f122503081d3243b480f398a787f6266c1
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-05 11:27:31 -0600

    Use DB_REPLICA

--- GlobalQueries.php
+++ GlobalQueries.php
@@ -10,8 +10,8 @@ class GlobalQueries {
 	public static function numberOfUsersInGlobalGroup( Parser $parser, String $group = '' ) {
 		$config = MediaWikiServices::getInstance()->getMainConfig();
 
-		$dbw = wfGetDB( DB_MASTER, [], $config->get( 'CentralAuthDatabase' ) );
+		$dbr = wfGetDB( DB_REPLICA, [], $config->get( 'CentralAuthDatabase' ) );
 	
-		return $dbw->selectRowCount( 'global_user_groups', '*', [ 'gug_group' => strtolower( $group ) ] );
+		return $dbr->selectRowCount( 'global_user_groups', '*', [ 'gug_group' => strtolower( $group ) ] );
 	}
 }
